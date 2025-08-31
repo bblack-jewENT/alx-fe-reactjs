8e12dde410c11a76db5d685eb800981813847c4f
Moses Rametsi <mosesrametsi16@gmail.com>
2025-09-01 00:04:21 +0200

    Point to relevant file.

---
+++ alx-react-app/src/MainContent.jsx
@@ -0,0 +1,11 @@
+import MainContent from './src/MainContent.jsx';
+
+function MainContent() {
+    return (
+        <main>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    )
+};
+
+export default MainContent;
