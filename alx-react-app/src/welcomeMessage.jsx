b46d7e625fb294edc13139bda5ee5be172356ec5
Moses Rametsi <mosesrametsi16@gmail.com>
2025-08-31 23:11:37 +0200

    Debug react code.

--- alx-react-app/src/welcomeMessage.jsx
+++ alx-react-app/src/welcomeMessage.jsx
@@ -6,8 +6,8 @@ function WelcomeMessage() {
             <h1>Hello everyone, I am learning React at ALX!</h1>
             <p>This is a simple JSX component.</p>
             <p>I am learning about JSX!</p>
+            <WelcomeMessage />
         </div>
-        <WelcomeMessage />
     );  
 }
 
