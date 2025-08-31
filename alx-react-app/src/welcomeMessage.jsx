cb62540a55de46512a3e62ed75c40e0b9f5ee434
Moses Rametsi <158572657+bblack-jewENT@users.noreply.github.com>
2025-08-31 22:37:02 +0200

    Update welcomeMessage.jsx
    Add a return statement.

--- alx-react-app/src/welcomeMessage.jsx
+++ alx-react-app/src/welcomeMessage.jsx
@@ -1,3 +1,9 @@
 function welcomeMessage() {
-  
+    return (
+        <div>
+            <h1>Welcome to ALX React APP!</h1>
+            <p>This is a simple JSX component.</p>
+        </div>
+    );  
 }
+
