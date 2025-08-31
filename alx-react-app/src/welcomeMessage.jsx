6ecaa32142908182bf0b68c63c7b111fad98303c
Moses Rametsi <mosesrametsi16@gmail.com>
2025-08-31 23:41:19 +0200

    Remove function code.

--- alx-react-app/src/welcomeMessage.jsx
+++ alx-react-app/src/welcomeMessage.jsx
@@ -11,14 +11,6 @@ function WelcomeMessage() {
     );  
 }
 
-function Header.jsx() {
-    return (
-        <header>
-          <h1>My Favorite Cities</h1>
-        </header>
-    )
-}
-
 export default WelcomeMessage;
 
 
