464078d6490e635d11f9da0483823abcab19f405
Moses Rametsi <mosesrametsi16@gmail.com>
2025-09-07 00:38:40 +0200

    Style header component.

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -1,11 +1,17 @@
-
 function Header() {
-    return (
-        <header>
-          <h1>My Favorite Cities</h1>
-        </header>
-    )
+  return (
+    <header>
+      <h1
+        style={{
+          color: "lightblue",
+          fontFamily: "poppins",
+          textAlign: "center",
+        }}
+      >
+        My Favorite Cities
+      </h1>
+    </header>
+  );
 }
 
 export default Header;
-
