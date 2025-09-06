46a801c96be59bb1c74025beaf495a3aa3012c28
Moses Rametsi <mosesrametsi16@gmail.com>
2025-09-07 00:37:19 +0200

    Fix code format.

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -11,7 +11,6 @@ import UserProfile from "./components/UserProfile";
 function App() {
   return (
     <>
-      <WelcomeMessage />
       <Header />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <MainContent />
