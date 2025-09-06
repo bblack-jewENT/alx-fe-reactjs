256adf4f2bdc9dcc8c609df439f77567c4b773f5
Moses Rametsi <mosesrametsi16@gmail.com>
2025-09-07 00:54:55 +0200

    Add css for the user profile component.

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,9 +1,36 @@
 const UserProfile = (props) => {
   return (
     <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+      <h2
+        style={{
+          color: "lightblue",
+          fontFamily: "poppins",
+          textDecoration: "underline",
+        }}
+      >
+        {props.name}
+      </h2>
+      <p
+        style={{
+          fontFamily: "poppins",
+          border: "1px solid lightblue",
+          padding: "5px",
+          color: "lightblue",
+          fontSize: "20px",
+        }}
+      >
+        Age: {props.age}
+      </p>
+      <p
+        style={{
+          color: "lightblue",
+          fontFamily: "poppins",
+          letterSpacing: "2px",
+          fontWeight: "bold",
+        }}
+      >
+        Bio: {props.bio}
+      </p>
     </div>
   );
 };
