9e8a474df1f0bd40c0b12e3b8555d7abb7331423
Moses Rametsi <mosesrametsi16@gmail.com>
2025-09-07 01:37:08 +0200

    Remove and import a new component.

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,8 +3,8 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./components/Header";
-import WelcomeMessage from "./components/WelcomeMessage";
 import MainContent from "./components/MainContent";
+import Counter from "./components/Counter";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
 
