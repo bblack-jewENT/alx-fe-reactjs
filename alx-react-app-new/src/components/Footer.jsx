622b12b4bbfe84776b7dcf5e0e18da85fb830b7a
Moses Rametsi <mosesrametsi16@gmail.com>
2025-09-07 18:25:31 +0200

    Add css to the footer component.

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,10 +1,15 @@
-
 function Footer() {
-    return (
-        <footer>
-            <p>© 2025 City Lovers</p>
-        </footer>
-    )
+  return (
+    <footer
+      style={{
+        backgroundColor: "#000",
+        color: "#fff",
+        padding: "30px auto",
+      }}
+    >
+      <p style={{ padding: "15px 0" }}>© 2025 City Lovers</p>
+    </footer>
+  );
 }
 
 export default Footer;
