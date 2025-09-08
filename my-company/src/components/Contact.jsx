4d6b4fcae6db4fcc7f3d4e9566d1abcaffc74588
Moses Rametsi <158572657+bblack-jewENT@users.noreply.github.com>
2025-09-08 14:24:30 +0200

    Update Contact.jsx
    Add a javascript function.

--- my-company/src/components/Contact.jsx
+++ my-company/src/components/Contact.jsx
@@ -1,2 +1,9 @@
 import React from 'react';
 import { useState } from 'react';
+
+ function Contact() {
+     const [formData, setFormData] = useState({
+       name: '',
+       email: '',
+       message: ''
+     });
