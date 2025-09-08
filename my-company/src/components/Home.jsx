55b6babf9cc85dff565e64c41eb4f9095899a1b1
Moses Rametsi <mosesrametsi16@gmail.com>
2025-09-08 15:43:37 +0200

    Append styles to the home page.

--- my-company/src/components/Home.jsx
+++ my-company/src/components/Home.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 
- function Home() {
-     return (
-       <div style={{ padding: '20px' }}>
-         <h1>Welcome to Our Company</h1>
-         <p>We are dedicated to delivering excellence in all our services.</p>
-       </div>
-     );
-   }
+function Home() {
+  return (
+    <div
+      style={{
+        padding: "40px",
+        textAlign: "center",
+        backgroundColor: "#f4f4f4",
+        minHeight: "60vh",
+        fontFamily: "Arial, sans-serif",
+      }}
+    >
+      <h1 style={{ color: "#333", marginBottom: "20px" }}>
+        Welcome to Our Company
+      </h1>
+      <p style={{ fontSize: "18px", color: "#666" }}>
+        We are dedicated to delivering excellence in all our services.
+      </p>
+    </div>
+  );
+}
 
 export default Home;
-
