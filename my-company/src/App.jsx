e2f657acb2fef172d0bbd082c5894089b84569a2
Moses Rametsi <mosesrametsi16@gmail.com>
2025-09-08 15:48:26 +0200

    Add component.

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,6 +1,23 @@
-import { useState } from "react";
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
 
-function App() {}
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
