834cdffb247aa0ec9a0c0fe15a5c038b3eb88b0b
Moses Rametsi <158572657+bblack-jewENT@users.noreply.github.com>
2025-09-08 14:33:06 +0200

    Update Contact.jsx
    Remove code.

--- my-company/src/components/Contact.jsx
+++ my-company/src/components/Contact.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 
- function Contact() {
-     const [formData, setFormData] = useState({
-       name: '',
-       email: '',
-       message: ''
-     });
 
-     const handleChange = (e) => {
-       setFormData({ ...formData, [e.target.name]: e.target.value });
-     };
+
+    
+
