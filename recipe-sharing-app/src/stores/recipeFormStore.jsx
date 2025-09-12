53ef96fd9e325985c8553b37a1cc8ddc3c948fbb
Moses Rametsi <mosesrametsi16@gmail.com>
2025-09-12 20:23:10 +0200

    Add inline styles and change relative path.

--- recipe-sharing-app/src/stores/recipeFormStore.jsx
+++ recipe-sharing-app/src/stores/recipeFormStore.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react';
-import { useRecipeStore } from './recipeStore';
+import { useState } from "react";
+import { useRecipeStore } from "./store";
 
-  const AddRecipeForm = () => {
-    const addRecipe = useRecipeStore(state => state.addRecipe);
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
+const AddRecipeForm = () => {
+  console.log("useRecipeStore in AddRecipeForm:", useRecipeStore);
+  const addRecipe = useRecipeStore((state) => state.addRecipe);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      addRecipe({ id: Date.now(), title, description });
-      setTitle('');
-      setDescription('');
-    };
-
-    return (
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          placeholder="Title"
-        />
-        <textarea
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          placeholder="Description"
-        />
-        <button type="submit">Add Recipe</button>
-      </form>
-    );
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addRecipe({ id: Date.now(), title, description });
+    setTitle("");
+    setDescription("");
   };
 
-export default AddRecipeForm;
+  return (
+    <form
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "1rem",
+      }}
+      onSubmit={handleSubmit}
+    >
+      <input
+        style={{
+          height: "45px",
+        }}
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Title"
+      />
+      <textarea
+        style={{
+          resize: "none",
+          height: "45px",
+        }}
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        placeholder="Description"
+      />
+      <button type="submit">Add Recipe</button>
+    </form>
+  );
+};
 
+export default AddRecipeForm;
