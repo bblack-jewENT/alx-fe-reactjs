54db162d69dc3533fd8a31e71155382472bff260
Moses Rametsi <mosesrametsi16@gmail.com>
2025-09-12 23:32:22 +0200

    Add relative path.

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,16 +1,25 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RecipeList from "./components/recipeList";
 import AddRecipeForm from "./components/recipeForm";
+import RecipeDetails from "./components/recipeDetails";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <RecipeList />
-      <AddRecipeForm />
-    </>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <RecipeList />
+              <AddRecipeForm />
+            </>
+          }
+        />
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+      </Routes>
+    </Router>
   );
 }
 
