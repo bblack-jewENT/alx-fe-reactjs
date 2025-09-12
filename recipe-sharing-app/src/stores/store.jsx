3d1f95cc34e00c0b21b615f207d1b344dd0c1e38
Moses Rametsi <158572657+bblack-jewENT@users.noreply.github.com>
2025-09-12 18:28:19 +0200

    Update store.jsx
    Import.

--- recipe-sharing-app/src/stores/store.jsx
+++ recipe-sharing-app/src/stores/store.jsx
@@ -1,7 +1,9 @@
 import create from 'zustand';
+import { useRecipeStore } from './recipeStore';
 
 const useRecipeStore = create(set => ({
   recipes: [],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes })
 }));
+
