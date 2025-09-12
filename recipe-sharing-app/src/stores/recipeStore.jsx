db8819d791b3604c99fc6fcef12203a80393f584
Moses Rametsi <mosesrametsi16@gmail.com>
2025-09-12 18:35:20 +0200

    Rename file name.

---
+++ recipe-sharing-app/src/stores/recipeStore.jsx
@@ -0,0 +1,27 @@
+import create from 'zustand';
+import { useRecipeStore } from './recipeStore';
+
+const useRecipeStore = create(set => ({
+  recipes: [],
+  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes })
+}));
+
+ const RecipeList = () => {
+    const recipes = useRecipeStore(state => state.recipes);
+
+    return (
+      <div>
+        {recipes.map(recipe => (
+          <div key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+export default RecipeList;
+
+
