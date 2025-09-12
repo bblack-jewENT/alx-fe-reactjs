530ead9c2d4d2431b12e2b74de56fa133e4d7b89
Moses Rametsi <mosesrametsi16@gmail.com>
2025-09-12 23:37:27 +0200

    Add react router.

--- recipe-sharing-app/src/components/recipeDetails.jsx
+++ recipe-sharing-app/src/components/recipeDetails.jsx
@@ -1,17 +1,43 @@
-import { useRecipeStore } from "../stores/recipeStore";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useRecipeStore } from "../stores/store";
+import EditRecipeForm from "./EditRecipeForm";
+import DeleteRecipeButton from "./DeleteRecipeButton";
 
-const RecipeDetails = ({ recipeId }) => {
-    const recipe = useRecipeStore(state =>
-      state.recipes.find(recipe => recipe.id === recipeId)
-    );
+const RecipeDetails = () => {
+  const { id } = useParams();
+  const recipe = useRecipeStore((state) =>
+    state.recipes.find((recipe) => recipe.id === parseInt(id))
+  );
 
-    return (
-      <div>
-        <h1>{recipe.title}</h1>
-        <p>{recipe.description}</p>
-        {/* Render EditRecipeForm and DeleteRecipeButton here */}
-      </div>
-    );
+  const [isEditing, setIsEditing] = useState(false);
+
+  if (!recipe) {
+    return <div>Recipe not found</div>;
   }
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleSave = () => {
+    setIsEditing(false);
+  };
+
+  return (
+    <div>
+      {isEditing ? (
+        <EditRecipeForm recipeId={recipe.id} onSave={handleSave} />
+      ) : (
+        <div>
+          <h1>{recipe.title}</h1>
+          <p>{recipe.description}</p>
+          <button onClick={handleEdit}>Edit</button>
+          <DeleteRecipeButton recipeId={recipe.id} />
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default RecipeDetails;
