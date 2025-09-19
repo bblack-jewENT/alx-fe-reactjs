6c10233e6179e9774ab9fa82210e3825de8dc600
Moses Rametsi <158572657+bblack-jewENT@users.noreply.github.com>
2025-09-19 12:14:07 +0200

    Update githubApi.js
    Append user details logic.

--- github-user-search/src/services/githubApi.js
+++ github-user-search/src/services/githubApi.js
@@ -13,3 +13,17 @@ export const searchUsers = async (query) => {
   }
   return response.json();
 };
+
+export const getUserDetails = async (username) => {
+  const apiKey = import.meta.env.VITE_APP_GITHUB_API_KEY;
+  const response = await fetch(`https://api.github.com/users/${username}`, {
+    headers: {
+      ...(apiKey && { Authorization: `token ${apiKey}` }),
+    },
+  });
+  if (!response.ok) {
+    throw new Error("Failed to fetch user details");
+  }
+  return response.json();
+};
+
