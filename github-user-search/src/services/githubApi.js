7552f77e173e4744a968e559d51189c052bebe05
Moses Rametsi <mosesrametsi16@gmail.com>
2025-09-20 00:11:54 +0200

    Add search import and more js logic.

--- github-user-search/src/services/githubApi.js
+++ github-user-search/src/services/githubApi.js
@@ -1,3 +1,23 @@
+import axios from "axios";
+
+export const fetchUserData = async (username) => {
+  const apiKey = import.meta.env.VITE_APP_GITHUB_API_KEY;
+
+  try {
+    const response = await axios.get(
+      `https://api.github.com/users/${username}`,
+      {
+        headers: {
+          ...(apiKey && { Authorization: `token ${apiKey}` }),
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed to fetch user data");
+  }
+};
+
 export const searchUsers = async (query) => {
   const apiKey = import.meta.env.VITE_APP_GITHUB_API_KEY;
 
