0c43b73ebd423c8c6d3d15cb164c7f1acb8485d7
Moses Rametsi <158572657+bblack-jewENT@users.noreply.github.com>
2025-09-19 12:31:05 +0200

    Update App.jsx
    Remove code and import css and react router dom.

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,23 +1,2 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import WelcomeMessage from "./components/WelcomeMessage";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
-import UserProfile from "./components/UserProfile";
-
-function App() {
-  return (
-    <>
-      <WelcomeMessage />
-      <Header />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      <MainContent />
-      <Footer />
-    </>
-  );
-}
-
-export default App;
