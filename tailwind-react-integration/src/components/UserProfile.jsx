2f8070ee0fb53e99d62485a7fc36e5f6e2107767
Moses Rametsi <158572657+bblack-jewENT@users.noreply.github.com>
2025-09-23 23:36:25 +0200

    Update UserProfile.jsx
    Add shadow effect.

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 function UserProfile() {
   return (
     <div>
-      <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg">
+      <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
         <img src="https://via.placeholder.com/150" alt="User" />
         <h1>John Doe</h1>
         <p>Developer at Example Co. Loves to write code and explore new technologies.</p>
@@ -11,6 +11,7 @@ function UserProfile() {
 };
 
 export default UserProfile;
+
 
 
 
