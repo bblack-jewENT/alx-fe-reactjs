8f7994407ccd7fa4308093e5e96b24ae2a35d258
Moses Rametsi <158572657+bblack-jewENT@users.noreply.github.com>
2025-09-23 23:32:48 +0200

    Update UserProfile.jsx
    Add inline tailwind css.

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 function UserProfile() {
   return (
     <div>
-      <div className="user-profile">
+      <div className="bg-gray-100 p-8">
         <img src="https://via.placeholder.com/150" alt="User" />
         <h1>John Doe</h1>
         <p>Developer at Example Co. Loves to write code and explore new technologies.</p>
@@ -11,3 +11,4 @@ function UserProfile() {
 };
 
 export default UserProfile;
+
