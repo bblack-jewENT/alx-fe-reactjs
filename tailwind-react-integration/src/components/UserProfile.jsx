076a66174c596d65872541bb747734c3b1ffc4fe
Moses Rametsi <158572657+bblack-jewENT@users.noreply.github.com>
2025-09-24 14:29:33 +0200

    Update UserProfile.jsx
    Add more responsiveness to the width.

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 function UserProfile() {
   return (
     <div>
-      <div className="bg-gray-100 sm:p-4 md:p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+      <div className="bg-gray-100 sm:p-4 md:p-8 max-w-sm max-w-xs mx-auto my-20 rounded-lg shadow-lg">
         <img className="rounded-full w-36 h-36 mx-auto" src="https://via.placeholder.com/150" alt="User" />
         <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
         <p className="text-gray-600 text-base">
@@ -14,6 +14,7 @@ function UserProfile() {
 }
 
 export default UserProfile;
+
 
 
 
