0b05e601513f56eac1a8736fc7c3972f0259cdfb
Moses Rametsi <158572657+bblack-jewENT@users.noreply.github.com>
2025-09-24 15:00:32 +0200

    Add new image.

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,11 @@ function UserProfile() {
   return (
     <div>
       <div className="bg-gray-100 sm:p-4 md:p-8 md:max-w-sm max-w-xs mx-auto my-20 rounded-lg shadow-lg">
-        <img className="rounded-full  sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto" src="https://via.placeholder.com/150" alt="User" />
+        <img
+          className="rounded-full  sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"
+          src="https://images.stockcake.com/public/e/e/6/ee6071b7-ef42-45e4-ba8c-9fc44bbdfa22_large/coding-at-night-stockcake.jpg"
+          alt="User"
+        />
         <h1 className="sm:text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
         <p className="text-gray-600 sm:text-sm md:text-base">
           Developer at Example Co. Loves to write code and explore new
@@ -14,16 +18,3 @@ function UserProfile() {
 }
 
 export default UserProfile;
-
-
-
-
-
-
-
-
-
-
-
-
-
