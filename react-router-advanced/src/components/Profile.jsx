9e22f83fa60ec5626d97ca280ffb93ca24003120
Moses Rametsi <158572657+bblack-jewENT@users.noreply.github.com>
2025-10-09 00:32:53 +0200

    Append directory and new jsx files with components.

---
+++ react-router-advanced/src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
+function Profile() {
+  return (
+    <>
+      <h1>Profile 🚻</h1>
+    </>
+  );
+}
+
+export default Profile;
---
+++ react-router-advanced/src/components/ProfileDetails.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
+function ProfileDetails() {
+  return (
+    <>
+      <h1>Profile Details</h1>
+    </>
+  );
+}
