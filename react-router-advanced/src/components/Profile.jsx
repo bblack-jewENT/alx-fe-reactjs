53376a3fbfceaa5df2579093f045e24b74980174
Moses Rametsi <158572657+bblack-jewENT@users.noreply.github.com>
2025-10-09 01:17:17 +0200

    Append imports and paths.

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails.jsx";
+import ProfileSettings from "./ProfileSettings.jsx";
 
 function Profile() {
   return (
     <>
       <h1>Profile 🚻</h1>
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
+      <Outlet />
     </>
   );
 }
