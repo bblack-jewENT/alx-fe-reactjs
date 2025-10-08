4994b70b5700fb0e86219bdbba4432e48c3b516e
Moses Rametsi <158572657+bblack-jewENT@users.noreply.github.com>
2025-10-09 01:15:40 +0200

    More imports and route logic.

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,15 +1,25 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Profile from "./components/Profile.jsx";
+import BlogPost from "./components/BlogPost.jsx";
+import Login from "./components/Login.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/profile-details" element={<ProfileDetails />} />
-        <Route path="/profile-settings" element={<ProfileSettings />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/posts/:id" element={<BlogPost />} />
       </Routes>
     </>
   );
